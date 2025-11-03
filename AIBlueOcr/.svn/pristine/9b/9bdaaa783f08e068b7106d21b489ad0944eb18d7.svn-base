1dc6bc9705eb11fef2fa2988e9265316e74f225f
Song-Chang-Hwa <songchanghwa64@gmail.com>
2025-11-03 22:24:47 +0900

    Delete AIBlueOcr directory

--- AIBlueOcr/.svn/pristine/9b/9bdaaa783f08e068b7106d21b489ad0944eb18d7.svn-base
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var app = angular.module('sbAdminApp');
-
-app.controller('IntroController', function ($scope, $http, $timeout, $compile, _) {
-	
-});
--- AIBlueOcr/ai_blue_ocr_server/src/main/webapp/ng-app/web/menu/menu.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-var app = angular.module('sbAdminApp', ['ui.grid', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.grid.exporter', 'ngResource', 'ui.grid.treeView', 'ui.grid.edit']);
